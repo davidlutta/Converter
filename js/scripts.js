371b6cd1b76e5208a655ccbcfd2ecafef6c4851b
davidlutta <davidlutta2010@gmail.com>
2018-03-20 17:04:59 +0300

    Added content to page

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("You are about to convert gallons to Liters");
+var result = function(number1) {
+  return number1 * 3.78541
+}
+var number1 =parseInt(prompt("How many gallons do you want to convert"));
+
+alert(result(number1))
